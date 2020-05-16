4feaf0dfca61ccf82bb72839b1164a1879305102
Ritik Gupta <guptaritik349@gmail.com>
2020-05-16 20:03:48 +0530

    Created Keeper App Repository

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+//jshint esversion:6
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className = "note">
+      <h1>Heading</h1>
+      <p>This is a paragraph</p>
+    </div>
+  );
+}
+
+export default Note;
