49e0b788c0a202fdf6d11bab904fbf8ece7c0ad2
Ritik Gupta <guptaritik349@gmail.com>
2020-06-05 14:25:17 +0530

    Updated Package.json

---
+++ build/precache-manifest.2495dc388ced278b6c1a19f9caa62516.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac61713c5dea8d0364c9751b3adc34f",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "2663be227dedc6d9fcd6",
+    "url": "/keeper-app/static/js/2.d118bcb9.chunk.js"
+  },
+  {
+    "revision": "70ed991a9032a2433e3b",
+    "url": "/keeper-app/static/js/main.34ebdd5a.chunk.js"
+  },
+  {
+    "revision": "67b5fba9d1dcb6a16278",
+    "url": "/keeper-app/static/js/runtime-main.de31b244.js"
+  }
+]);
